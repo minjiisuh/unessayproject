f90d08e125a8315e8c5c4dff6db16436ecd22b56
minjiisuh <145401386+minjiisuh@users.noreply.github.com>
2025-03-19 15:14:37 -0500

    Update unessay_project.py

--- unessay_project.py
+++ unessay_project.py
@@ -7,11 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1YwNL9yop9ptgDz7A6jB6C5DDm4yBCJ3u
 """
 
-
-
-!pip install pymupdf
-!pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
